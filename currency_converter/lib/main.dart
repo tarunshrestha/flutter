7ca119c2e408376372354ed07db8975a0de7c34f
tarunshrestha <tarunshrestha90@gmail.com>
2025-05-04 21:38:46 +0545

    Drop

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Text(
-      "Hello",
+      "Hello k xa",
       textDirection: TextDirection.ltr,
     );
   }
