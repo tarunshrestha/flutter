76231332c19c886a5c9f94778614f69bb032d885
tarunshrestha <tarunshrestha90@gmail.com>
2025-05-11 21:15:10 +0545

    search bar

--- todoapp/lib/main.dart
+++ todoapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todoapp/screens/home.dart';
 
 void main() {
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Todo", home: Home());
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, title: "Todo", home: Home());
   }
 }
--- todoapp/lib/screens/home.dart
+++ todoapp/lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -6,9 +7,50 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Todoapp')),
+      backgroundColor: tdBGColor,
+      appBar: _buildApp(),
       body: Container(
-        child: Text("This is home screen"),
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [searchBox()],
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildApp() {
+    return AppBar(
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Icon(Icons.menu, color: tdBlack, size: 30),
+          Container(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/images/G.png')))
+        ]));
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+              Icons.search,
+              color: tdBlack,
+              size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 20),
+            border: InputBorder.none,
+            hintText: "Search",
+            hintStyle: TextStyle(color: tdGrey)),
       ),
     );
   }
