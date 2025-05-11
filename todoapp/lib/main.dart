5b057f5c4c957934b84d2977895a78d740072578
tarunshrestha <tarunshrestha90@gmail.com>
2025-05-11 20:42:03 +0545

    Todo start

---
+++ todoapp/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Todo", home: Home());
+  }
+}
